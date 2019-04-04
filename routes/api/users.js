6713cfa440d97c913cf203c988d640b3c023d936
Joseph Akayesi <josephakayesi@gmail.com>
2019-04-04 20:27:17 +0000

    Code restructuring

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,47 @@
 const router = require('express').Router()
+const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
+
+// Load User model
+const User =  require('../../models/User')
 
 // @route   GET api/users/test
 // @desc    Test users route
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Users route works' }))
+
+// @route   POST api/users/register
+// @desc    Register user
+// @access  Public
+router.post('/register', (req, res) => {
+    User.findOne({email: req.body.email})
+        .then(user => {
+            if(user){
+                return res.status(400).json({email: 'Email already exists'})
+            }
+            else {
+                const avatar = gravatar.url(req.body.email, {
+                    s: '200',
+                    r: 'pg',
+                    d: 'mm'
+                })
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar,
+                    password: req.body.password
+                }) 
+
+                bcrypt.genSalt(12, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err
+                        newUser.password = hash
+                        newUser.save()
+                            .then(user => res.json(user))
+                    })
+                })
+            }
+        })
+})
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const app = require('express')()
 const mongoose = require('mongoose')
-const users = require('../routes/api/users')
+const bodyParser = require('body-parser')
+
+const users = require('./routes/api/users')
+
+// Body Parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // Db config
 const db = require('../config/keys').mongoURI
@@ -9,11 +15,11 @@ const db = require('../config/keys').mongoURI
 mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
     .then(() => console.log('MongoDB connected'))
     .catch(() => console.log(err))
-    app.get('/', (req, res) => res.send('Hello'))
+app.get('/', (req, res) => res.send('Hello'))
 
 // Use routes
 app.use('/api/users', users)
 
-const port = process.env.PORT || 5000 
+const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
