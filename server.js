f80e9f34c61c913b5c70c50ac38ce718bdde702d
Joseph Akayesi <josephakayesi@gmail.com>
2019-04-13 20:52:05 +0000

    Readded config

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require('express')()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const passport = require('passport')
+const keys = require('./config/keys')
 const path = require('path')
 
 const users = require('./routes/api/users')
