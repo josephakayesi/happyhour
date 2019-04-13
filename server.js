81c77358530b4771d1269bbf440b924d93a6957f
Joseph Akayesi <josephakayesi@gmail.com>
2019-04-13 20:39:28 +0000

    Removed uneccessary imports

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('express')()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const passport =  require('passport')
-const keys = require('./config/keys')
+const path = require('path')
 
 const users = require('./routes/api/users')
 
@@ -28,6 +28,16 @@ app.get('/', (req, res) => res.send('Hello'))
 
 // Use routes
 app.use('/api/users', users)
+
+// Server stati assets if in production
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.senFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const port = process.env.PORT || 5000
 
