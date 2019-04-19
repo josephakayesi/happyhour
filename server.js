80c1908fdb23ca8e7ddf18bace838dceb2728152
Joseph Akayesi <josephakayesi@gmail.com>
2019-04-19 12:30:03 +0000

    Removed route to path and left route to path * for static folder

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use('/api/users', users)
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use('*', express.static('client/build'))
-    app.use(express.static('client/build'))
+    // app.use(express.static('client/build'))
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
